8879c0ddeb0ed9cbf1996ce7b9ab42815c0b7353
Santiago Andres Gelvez Camargo <santiagogelvez29@gmail.com>
2021-06-28 17:55:20 -0500

    commit inicial con los primeros retos

---
+++ conversor.py
@@ -0,0 +1,37 @@
+menu = """
+Bienvenidos al conversor de monedas 💰
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1 :
+    pesos = input("¿Cuantos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == 2 :
+    pesos = input("¿Cuantos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == 3 :
+    pesos = input("¿Cuantos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+else:
+    print("Ingresa una opción correcta por favor")
---
+++ reto-conversor.py
@@ -0,0 +1,3 @@
+dolares = float(input("¿Cuantos dolares tienes?: "))
+pesos = str(round(dolares * 3875, 2))
+print("Tienes $" + pesos + " pesos colombianos")
