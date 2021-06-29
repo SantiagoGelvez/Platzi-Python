5b310cd54b5505d5215b47457d6f386e82e26881
Santiago Andres Gelvez Camargo <santiagogelvez29@gmail.com>
2021-06-29 18:30:28 -0500

    Explicacion del tema para los ciclos while y for

---
+++ for.py
@@ -0,0 +1,17 @@
+# Los numeros del uno al mil
+
+# con ciclo while
+
+# contador = 1
+# print(contador)
+# while contador < 1000:
+#     contador += 1 # contador = contador + 1
+#     print(contador)
+
+# con ciclo for
+
+for contador in range(1,1001):
+    print(contador)
+
+for i in range(10):
+    print(11 * i)
