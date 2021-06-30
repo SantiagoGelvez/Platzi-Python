04c8ada2ef6b8906e9074ede8fffb7bb0cca1b85
Santiago Andres Gelvez Camargo <santiagogelvez29@gmail.com>
2021-06-30 00:21:54 -0500

    Ejercicios para entender las sentencias break y continue en los bucles

---
+++ reto-break-continue.py
@@ -0,0 +1,25 @@
+def run():
+    enunciado = '''
+    Leer números enteros positivos de teclado, hasta que el usuario ingrese el 0. 
+    Informar cuál fue el mayor número ingresado.
+    '''
+    print(enunciado)
+    mayor = 0
+    while True:
+        numero = int(input('Ingrese un numero: '))
+        if numero == 0:
+            print('Fin del programa')
+            print('El numero mayor ingresado fue ' + str(mayor))
+            break
+        elif numero < 0:
+            print('Numero incorrecto. Debe ser positivo')
+            continue
+        else:
+            if numero > mayor:
+                mayor = numero
+            continue
+
+
+
+if __name__ == '__main__':
+    run()
